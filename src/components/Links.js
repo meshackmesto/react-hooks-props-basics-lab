6b7ccd4da795cf9b69bde45dfd3cc338f403e85b
meshackmesto <meshackorina93@gmail.com>
2024-04-28 06:46:27 +0300

    completed

---
+++ src/components/Links.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Links({linkedin, github}){
+    return (
+        <div>
+        <h3>Links</h3>,
+        <a href="https://github.com/liza">Github</a>,
+        <a href="https://www.linkedin.com/in/liza/">Linkedin</a>
+        </div>
+    );
+}
+
+export default Links;
